9fb35dbd1f92521c7449bcb2bcc051cf98683acf
Simon Lignac <tonmail@example.com>
2026-03-17 09:21:40 +0100

    Ajout du fichier cpp

---
+++ Simonsystinfo/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+
+// Déclaration de la fonction
+int addition(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int x = 5;
+    int y = 3;
+
+    int resultat = addition(x, y);
+
+    cout << "Le resultat est : " << resultat << endl;
+
+    return 0;
+}
